b5897ffedbb9a220cd00af612a530e82e92c56bb
gdpettus <gabe.pettus@gmail.com>
2019-10-16 21:18:29 -0600

    pseudo code outline

---
+++ script.js
@@ -0,0 +1,12 @@
+// pull current day
+// pull current location
+
+// function to get weather for day at location
+
+// function to draw  weather all days
+ // need logic to pick variables
+ // function to color uv index
+
+// function to pull city history from local memory
+// function to update current weather
+// function to update 5 day weather
